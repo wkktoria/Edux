5661a6924c15087fcea9ab198bc3e79e5b7021a2
Wiktoria Peretiatkowicz <127852624+wkktoria@users.noreply.github.com>
2024-06-12 22:26:12 +0200

    Initialize project

---
+++ src/main/java/io/github/wkktoria/edux/EduxApplication.java
@@ -0,0 +1,13 @@
+package io.github.wkktoria.edux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EduxApplication.class, args);
+    }
+
+}
