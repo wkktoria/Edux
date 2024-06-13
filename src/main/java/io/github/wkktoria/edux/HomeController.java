bc70a436007c14b6dc44a1883e10991fa35d1cdf
Wiktoria Peretiatkowicz <127852624+wkktoria@users.noreply.github.com>
2024-06-13 11:21:13 +0200

    Add Thymeleaf

--- src/main/java/io/github/wkktoria/edux/HomeController.java
+++ src/main/java/io/github/wkktoria/edux/HomeController.java
@@ -1,12 +1,14 @@
 package io.github.wkktoria.edux;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 class HomeController {
     @RequestMapping(value = {"", "/", "/home"})
-    String displayHomePage() {
+    String displayHomePage(Model model) {
+        model.addAttribute("username", "Anna");
         return "home.html";
     }
 }
