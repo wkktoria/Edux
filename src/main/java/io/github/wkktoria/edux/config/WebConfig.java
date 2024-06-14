22c6aee8be49b6f3aa1e76bc9ed6dab5d88c6398
Wiktoria Peretiatkowicz <127852624+wkktoria@users.noreply.github.com>
2024-06-14 22:31:43 +0200

    Create courses page

---
+++ src/main/java/io/github/wkktoria/edux/config/WebConfig.java
@@ -0,0 +1,13 @@
+package io.github.wkktoria.edux.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addViewControllers(final ViewControllerRegistry registry) {
+        registry.addViewController("/courses").setViewName("courses");
+    }
+}
