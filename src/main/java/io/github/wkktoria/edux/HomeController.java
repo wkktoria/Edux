fb15411b69d4bbc99fd4d1626d6c00acb04d4b0b
Wiktoria Peretiatkowicz <127852624+wkktoria@users.noreply.github.com>
2024-06-14 22:32:08 +0200

    Clean up home controller

--- src/main/java/io/github/wkktoria/edux/HomeController.java
+++ src/main/java/io/github/wkktoria/edux/HomeController.java
@@ -1,14 +1,12 @@
 package io.github.wkktoria.edux;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 class HomeController {
     @RequestMapping(value = {"", "/", "/home"})
-    String displayHomePage(Model model) {
-        model.addAttribute("username", "Anna");
+    String displayHomePage() {
         return "home.html";
     }
 }
