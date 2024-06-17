5ae2cc5e7600568b3e09bec0cde95569ef9f3524
Wiktoria Peretiatkowicz <127852624+wkktoria@users.noreply.github.com>
2024-06-17 17:46:00 +0200

    Submit information from contact form

--- src/main/java/io/github/wkktoria/edux/controller/ContactController.java
+++ src/main/java/io/github/wkktoria/edux/controller/ContactController.java
@@ -1,12 +1,30 @@
 package io.github.wkktoria.edux.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 class ContactController {
+    private static final Logger logger = LoggerFactory.getLogger(ContactController.class);
+
     @RequestMapping("/contact")
     String displayContactPage() {
         return "contact.html";
+    }
+
+    @RequestMapping(value = "/saveMessage", method = RequestMethod.POST)
+    ModelAndView saveMessage(@RequestParam String name, @RequestParam String phoneNumber,
+                             @RequestParam String email, @RequestParam String subject, @RequestParam String message) {
+        logger.info("Name: {}", name);
+        logger.info("Phone number: {}", phoneNumber);
+        logger.info("Email: {}", email);
+        logger.info("Subject: {}", subject);
+        logger.info("Message: {}", message);
+        return new ModelAndView("redirect:/contact");
     }
 }
