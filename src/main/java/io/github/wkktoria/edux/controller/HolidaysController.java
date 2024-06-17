1a266ba83c64d6a72a364dfc3485e0b554e0059d
Wiktoria Peretiatkowicz <127852624+wkktoria@users.noreply.github.com>
2024-06-17 21:31:03 +0200

    Create holidays page

---
+++ src/main/java/io/github/wkktoria/edux/controller/HolidaysController.java
@@ -0,0 +1,33 @@
+package io.github.wkktoria.edux.controller;
+
+import io.github.wkktoria.edux.model.Holiday;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Controller
+class HolidaysController {
+    @GetMapping("/holidays")
+    String displayHolidays(Model model) {
+        List<Holiday> holidays = Arrays.asList(
+                new Holiday("Jan 1", "New Year's Day", Holiday.Type.FESTIVAL),
+                new Holiday("Oct 31", "Halloween", Holiday.Type.FESTIVAL),
+                new Holiday("Nov 24", "Thanksgiving Day", Holiday.Type.FESTIVAL),
+                new Holiday("Dec 25", "Christmas", Holiday.Type.FESTIVAL),
+                new Holiday("Jul 4", "Independence Day", Holiday.Type.FEDERAL),
+                new Holiday("Nov 11", "Veterans Day", Holiday.Type.FEDERAL)
+        );
+
+        Holiday.Type[] types = Holiday.Type.values();
+
+        for (Holiday.Type type : types) {
+            model.addAttribute(type.toString(), (holidays.stream().filter(holiday -> holiday.getType().equals(type)).collect(Collectors.toList())));
+        }
+
+        return "holidays.html";
+    }
+}
---
+++ src/main/java/io/github/wkktoria/edux/model/Holiday.java
@@ -0,0 +1,29 @@
+package io.github.wkktoria.edux.model;
+
+public class Holiday {
+    private final String day;
+    private final String reason;
+    private final Type type;
+
+    public Holiday(String day, String reason, Type type) {
+        this.day = day;
+        this.reason = reason;
+        this.type = type;
+    }
+
+    public String getDay() {
+        return day;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public enum Type {
+        FESTIVAL, FEDERAL
+    }
+}
