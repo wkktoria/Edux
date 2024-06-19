34e4b8dc42f01f566443a1bd17b6bbb490f81719
Wiktoria Peretiatkowicz <127852624+wkktoria@users.noreply.github.com>
2024-06-19 18:38:23 +0200

    Use Lombok

--- src/main/java/io/github/wkktoria/edux/model/Contact.java
+++ src/main/java/io/github/wkktoria/edux/model/Contact.java
@@ -1,60 +1,12 @@
 package io.github.wkktoria.edux.model;
 
+import lombok.Data;
+
+@Data
 public class Contact {
     private String name;
     private String phoneNumber;
     private String email;
     private String subject;
     private String message;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(final String name) {
-        this.name = name;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(final String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(final String email) {
-        this.email = email;
-    }
-
-    public String getSubject() {
-        return subject;
-    }
-
-    public void setSubject(final String subject) {
-        this.subject = subject;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(final String message) {
-        this.message = message;
-    }
-
-    @Override
-    public String toString() {
-        return "Contact{" +
-                "name='" + name + '\'' +
-                ", phoneNumber='" + phoneNumber + '\'' +
-                ", email='" + email + '\'' +
-                ", subject='" + subject + '\'' +
-                ", message='" + message + '\'' +
-                '}';
-    }
 }
