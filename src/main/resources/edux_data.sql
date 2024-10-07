aae00c85adf25769afc6ad1a859a1f03ddf904ad
Wiktoria Peretiatkowicz <127852624+wkktoria@users.noreply.github.com>
2024-10-07 16:22:19 +0200

    Format SQL scripts

--- src/main/resources/edux_data.sql
+++ src/main/resources/edux_data.sql
@@ -1,33 +1,37 @@
-use edux;
+USE edux;
 
-insert into `holidays` (`day`, `reason`, `type`, `created_at`, `created_by`)
-values ('Jan 1', 'New Year''s day', 'FESTIVAL', CURDATE(), 'DBA');
 
-insert into `holidays` (`day`, `reason`, `type`, `created_at`, `created_by`)
-values ('Oct 31', 'Halloween', 'FESTIVAL', CURDATE(), 'DBA');
+INSERT INTO `holidays` (`day`, `reason`, `type`, `created_at`, `created_by`)
+VALUES ('Jan 1', 'New Year''s day', 'FESTIVAL', CURDATE(), 'DBA');
 
-insert into `holidays` (`day`, `reason`, `type`, `created_at`, `created_by`)
-values ('Nov 24', 'Thanksgiving Day', 'FESTIVAL', CURDATE(), 'DBA');
 
-insert into `holidays` (`day`, `reason`, `type`, `created_at`, `created_by`)
-values ('Dec 25', 'Christmas', 'FESTIVAL', CURDATE(), 'DBA');
+INSERT INTO `holidays` (`day`, `reason`, `type`, `created_at`, `created_by`)
+VALUES ('Oct 31', 'Halloween', 'FESTIVAL', CURDATE(), 'DBA');
 
-insert into `holidays` (`day`, `reason`, `type`, `created_at`, `created_by`)
-values ('Jul 4', 'Independence Day', 'FEDERAL', CURDATE(), 'DBA');
 
-insert into `holidays` (`day`, `reason`, `type`, `created_at`, `created_by`)
-values ('Nov 11', 'Veterans Day', 'FEDERAL', CURDATE(), 'DBA');
+INSERT INTO `holidays` (`day`, `reason`, `type`, `created_at`, `created_by`)
+VALUES ('Nov 24', 'Thanksgiving Day', 'FESTIVAL', CURDATE(), 'DBA');
 
-insert into `role` (`role_name`, `created_at`, `created_by`)
-values ('ADMIN', CURDATE(), 'DBA');
 
-insert into `role` (`role_name`, `created_at`, `created_by`)
-values ('STUDENT', CURDATE(), 'DBA');
+INSERT INTO `holidays` (`day`, `reason`, `type`, `created_at`, `created_by`)
+VALUES ('Dec 25', 'Christmas', 'FESTIVAL', CURDATE(), 'DBA');
 
-insert into `person` (`name`, `email`, `phone_number`, `password`, `role_id`, `created_at`, `created_by`)
-values ('Admin', 'admin@edux.com', '1234567890', 'adminstr0ngp@sw00rd', 1, CURDATE(), 'DBA');
 
-delete from `person` where email='admin@edux.com';
+INSERT INTO `holidays` (`day`, `reason`, `type`, `created_at`, `created_by`)
+VALUES ('Jul 4', 'Independence Day', 'FEDERAL', CURDATE(), 'DBA');
 
-insert into `person` (`name`, `email`, `phone_number`, `password`, `role_id`, `created_at`, `created_by`)
-values ('Admin', 'admin@edux.com', '+1234567890', '$2a$12$6AQpapoZjc7LxZ7TZpLh8eKE./RlDD5IegKS0tP18LIO8nGOfjvFG', 1, CURDATE(), 'DBA');
+
+INSERT INTO `holidays` (`day`, `reason`, `type`, `created_at`, `created_by`)
+VALUES ('Nov 11', 'Veterans Day', 'FEDERAL', CURDATE(), 'DBA');
+
+
+INSERT INTO `role` (`role_name`, `created_at`, `created_by`)
+VALUES ('ADMIN', CURDATE(), 'DBA');
+
+
+INSERT INTO `role` (`role_name`, `created_at`, `created_by`)
+VALUES ('STUDENT', CURDATE(), 'DBA');
+
+
+INSERT INTO `person` (`name`, `email`, `phone_number`, `password`, `role_id`, `created_at`, `created_by`)
+VALUES ('Admin', 'admin@edux.com', '+1234567890', '$2a$12$6AQpapoZjc7LxZ7TZpLh8eKE./RlDD5IegKS0tP18LIO8nGOfjvFG', 1, CURDATE(), 'DBA');
--- src/main/resources/edux_schema.sql
+++ src/main/resources/edux_schema.sql
@@ -1,74 +1,64 @@
-create database edux;
+CREATE DATABASE edux;
 
-use edux;
+USE edux;
 
-create table if not exists `contact_message`
-(
-    `contact_id`   int auto_increment primary key,
-    `name`         varchar(100)  not null,
-    `phone_number` varchar(12)   not null,
-    `email`        varchar(50)   not null,
-    `subject`      varchar(100)  not null,
-    `message`      varchar(1000) not null,
-    `status`       varchar(10)   not null,
-    `created_at`   timestamp     not null,
-    `created_by`   varchar(50)   not null,
-    `updated_at`   timestamp   null default null,
-    `updated_by`   varchar(50) default null
-);
 
-create table if not exists `holidays`
-(
-    `day`        varchar(10)  not null,
-    `reason`     varchar(100) not null,
-    `type`       varchar(20)  not null,
-    `created_at` timestamp    not null,
-    `created_by` varchar(50)  not null,
-    `updated_at` timestamp   null default null,
-    `updated_by` varchar(50) default null
-);
+CREATE TABLE IF NOT EXISTS `contact_message` (`contact_id` int AUTO_INCREMENT PRIMARY KEY,
+                                              `name` varchar(100) NOT NULL,
+    `phone_number` varchar(12) NOT NULL,
+    `email` varchar(50) NOT NULL,
+    `subject` varchar(100) NOT NULL,
+    `message` varchar(1000) NOT NULL,
+    `status` varchar(10) NOT NULL,
+    `created_at` timestamp NOT NULL,
+    `created_by` varchar(50) NOT NULL,
+    `updated_at` timestamp NULL DEFAULT NULL,
+    `updated_by` varchar(50) DEFAULT NULL);
 
-create table if not exists `role`
-(
-    `role_id`    int         not null auto_increment,
-    `role_name`  varchar(25) not null,
-    `created_at` timestamp   not null,
-    `created_by` varchar(50) not null,
-    `updated_at` timestamp   null default null,
-    `updated_by` varchar(50) default null,
-    primary key (`role_id`)
-);
 
-create table if not exists `address`
-(
-    `address_id` int          not null auto_increment,
-    `address1`   varchar(200) not null,
-    `address2`   varchar(200) default null,
-    `city`       varchar(50)  not null,
-    `state`      varchar(50)  default null,
-    `zip_code`   varchar(12)  not null,
-    `country`    varchar(50)  not null,
-    `created_at` timestamp    not null,
-    `created_by` varchar(50)  not null,
-    `updated_at` timestamp    null default null,
-    `updated_by` varchar(50)  default null,
-    primary key (`address_id`)
-);
+CREATE TABLE IF NOT EXISTS `holidays` (`day` varchar(10) NOT NULL,
+    `reason` varchar(100) NOT NULL,
+    `type` varchar(20) NOT NULL,
+    `created_at` timestamp NOT NULL,
+    `created_by` varchar(50) NOT NULL,
+    `updated_at` timestamp NULL DEFAULT NULL,
+    `updated_by` varchar(50) DEFAULT NULL);
 
-create table if not exists `person`
-(
-    `person_id`    int          not null auto_increment,
-    `name`         varchar(100) not null,
-    `phone_number` varchar(12)  not null,
-    `email`        varchar(50)  not null,
-    `password`     varchar(200) not null,
-    `role_id`      int          not null,
-    `address_id`   int          null,
-    `created_at`   timestamp    not null,
-    `created_by`   varchar(50)  not null,
-    `updated_at`   timestamp   null default null,
-    `updated_by`   varchar(50) default null,
-    primary key (`person_id`),
-    foreign key (`role_id`) references role (`role_id`),
-    foreign key (`address_id`) references address (`address_id`)
-);
+
+CREATE TABLE IF NOT EXISTS `role` (`role_id` int NOT NULL AUTO_INCREMENT,
+                                   `role_name` varchar(25) NOT NULL,
+    `created_at` timestamp NOT NULL,
+    `created_by` varchar(50) NOT NULL,
+    `updated_at` timestamp NULL DEFAULT NULL,
+    `updated_by` varchar(50) DEFAULT NULL,
+    PRIMARY KEY (`role_id`));
+
+
+CREATE TABLE IF NOT EXISTS `address` (`address_id` int NOT NULL AUTO_INCREMENT,
+                                      `address1` varchar(200) NOT NULL,
+    `address2` varchar(200) DEFAULT NULL,
+    `city` varchar(50) NOT NULL,
+    `state` varchar(50) DEFAULT NULL,
+    `zip_code` varchar(12) NOT NULL,
+    `country` varchar(50) NOT NULL,
+    `created_at` timestamp NOT NULL,
+    `created_by` varchar(50) NOT NULL,
+    `updated_at` timestamp NULL DEFAULT NULL,
+    `updated_by` varchar(50) DEFAULT NULL,
+    PRIMARY KEY (`address_id`));
+
+
+CREATE TABLE IF NOT EXISTS `person` (`person_id` int NOT NULL AUTO_INCREMENT,
+                                     `name` varchar(100) NOT NULL,
+    `phone_number` varchar(12) NOT NULL,
+    `email` varchar(50) NOT NULL,
+    `password` varchar(200) NOT NULL,
+    `role_id` int NOT NULL,
+    `address_id` int NULL,
+    `created_at` timestamp NOT NULL,
+    `created_by` varchar(50) NOT NULL,
+    `updated_at` timestamp NULL DEFAULT NULL,
+    `updated_by` varchar(50) DEFAULT NULL,
+    PRIMARY KEY (`person_id`),
+    FOREIGN KEY (`role_id`) REFERENCES ROLE (`role_id`),
+    FOREIGN KEY (`address_id`) REFERENCES address (`address_id`));
