455814f3b80a1b3789320ec53f626c72d794e53f
Wiktoria Peretiatkowicz <127852624+wkktoria@users.noreply.github.com>
2024-10-18 20:37:43 +0200

    Use custom queries in contact repository

--- src/main/java/io/github/wkktoria/edux/repository/ContactRepository.java
+++ src/main/java/io/github/wkktoria/edux/repository/ContactRepository.java
@@ -3,6 +3,7 @@ package io.github.wkktoria.edux.repository;
 import io.github.wkktoria.edux.model.Contact;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,7 +11,9 @@ import java.util.List;
 
 @Repository
 public interface ContactRepository extends CrudRepository<Contact, Integer> {
+    @Query(value = "SELECT * FROM contact_message c WHERE c.status = :status", nativeQuery = true)
     List<Contact> findByStatus(final String status);
 
+    @Query("SELECT c FROM Contact c WHERE c.status = :status")
     Page<Contact> findByStatus(final String status, final Pageable pageable);
 }
