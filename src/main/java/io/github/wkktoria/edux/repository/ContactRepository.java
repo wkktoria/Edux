c300e46d09765ff5240df149aec6a14392e81e00
Wiktoria Peretiatkowicz <127852624+wkktoria@users.noreply.github.com>
2024-11-01 19:12:44 +0100

    Remove unused import

--- src/main/java/io/github/wkktoria/edux/repository/ContactRepository.java
+++ src/main/java/io/github/wkktoria/edux/repository/ContactRepository.java
@@ -6,7 +6,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
