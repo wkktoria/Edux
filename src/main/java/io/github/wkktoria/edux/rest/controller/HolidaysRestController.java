3db6dfea12addbd88a8227c149fd5ca93ce447bc
Wiktoria Peretiatkowicz <127852624+wkktoria@users.noreply.github.com>
2024-11-27 20:28:21 +0100

    Create holidays REST controller

---
+++ src/main/java/io/github/wkktoria/edux/rest/controller/HolidaysRestController.java
@@ -0,0 +1,42 @@
+package io.github.wkktoria.edux.rest.controller;
+
+import io.github.wkktoria.edux.model.Holiday;
+import io.github.wkktoria.edux.repository.HolidaysRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+@Slf4j
+@RestController
+@RequestMapping(path = "/api/holidays", produces = {
+        MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE
+})
+@CrossOrigin(origins = "*")
+public class HolidaysRestController {
+    private final HolidaysRepository holidaysRepository;
+
+    @Autowired
+    public HolidaysRestController(final HolidaysRepository holidaysRepository) {
+        this.holidaysRepository = holidaysRepository;
+    }
+
+    @GetMapping("/getHolidays")
+    List<Holiday> getHolidays() {
+        Iterable<Holiday> holidays = holidaysRepository.findAll();
+        return StreamSupport.stream(holidays.spliterator(), false).toList();
+    }
+
+    @GetMapping("/getHolidaysByType")
+    List<Holiday> getHolidaysByType(@RequestParam final String type) {
+        Iterable<Holiday> holidays = holidaysRepository.findAll();
+        List<Holiday> holidayList = StreamSupport.stream(holidays.spliterator(), false).toList();
+
+        return holidayList.stream().filter(holiday ->
+                holiday.getType().toString().equalsIgnoreCase(type)).collect(Collectors.toList());
+    }
+}
